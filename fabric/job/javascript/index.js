6c97269f3fe1c281d2662450cd7f0ee498ae6668
Daniel Febrero <febrero@me.com>
2020-09-03 17:05:03 +0200

    BLOCKOTUS/organism#16 trycatch to promise

--- fabric/job/javascript/index.js
+++ fabric/job/javascript/index.js
@@ -4,37 +4,10 @@
 
 'use strict';
 
-const { Gateway, Wallets } = require('fabric-network');
 const fs = require('fs');
 const path = require('path');
 
-const WALLET_PATH = path.join(__dirname, '..', '..', '..', '..', '..', 'wallet');
-const CCP_PATH = path.resolve(__dirname, '..', '..', '..', '..', '..', 'network', 'organizations', 'peerOrganizations', 'org1.example.com', 'connection-org1.json');
-
-async function getContractAndGateway({username, contract}) {
-	// load the network configuration
-	const ccp = JSON.parse(fs.readFileSync(CCP_PATH, 'utf8'));
-
-	// Create a new file system based wallet for managing identities.
-	const wallet = await Wallets.newFileSystemWallet(WALLET_PATH);
-
-	// Check to see if we've already enrolled the job.
-	const identity = await wallet.get(username);
-	if (!identity) {
-		console.log(`An identity for the job "${username}" does not exist in the wallet`);
-		return {};
-	}
-
-	// Create a new gateway for connecting to our peer node.
-	const gateway = new Gateway();
-	await gateway.connect(ccp, { identity, discovery: { enabled: true, asLocalhost: true } });
-
-	// Get the network (channel) our contract is deployed to.
-	const network = await gateway.getNetwork('mychannel');
-
-	// Get the contract from the network.
-	return {contract: network.getContract('job', contract), gateway};
-}
+const { getContractAndGateway } = require('../../../../helper/fabric/helper/javascript');
 
 async function create({
 	type,
@@ -44,26 +17,31 @@ async function create({
 	user
 }) {
 	// Submit the specified transafalsection.
-	try{		
+	return new Promise(async (resolve, reject) => {
 		// create wallet
 		const walletPath = path.join(__dirname, '../../../../../wallet', `${user.username}.id`);
 		fs.writeFileSync(walletPath, JSON.stringify(user.wallet))
 
 		// get contract, submit transaction and disconnect
-		const {contract, gateway} = await getContractAndGateway({username: user.username, contract: 'Job'});
+		const {contract, gateway} = await 
+			getContractAndGateway({username: user.username, chaincode: 'job', contract: 'Job'})
+				.catch(reject);
 		
 		// submit transaction
-		const rawWorkers = await contract.submitTransaction('createJob', type, data, chaincode, key);
+		const rawWorkers = await 
+			contract
+				.submitTransaction('createJob', type, data, chaincode, key)
+				.catch(reject);
+
 		const workers = JSON.parse(rawWorkers.toString('utf8'))
 		console.log('Transaction has been submitted');
 
 		//disconnect
 		await gateway.disconnect();
 
-		return workers;
-    }catch(e){ 
-		console.log('ERROR: ', e) 
-	}
+		resolve(workers);
+		return;
+    })
 }
 
 async function list({
@@ -71,26 +49,31 @@ async function list({
 	user
 }) {
 	// Submit the specified transafalsection.
-	try{		
+	return new Promise(async (resolve, reject) => {
 		// create wallet
 		const walletPath = path.join(__dirname, '../../../../../wallet', `${user.username}.id`);
 		fs.writeFileSync(walletPath, JSON.stringify(user.wallet))
 
 		// get contract, submit transaction and disconnect
-		const {contract, gateway} = await getContractAndGateway({username: user.username, contract: 'Job'});
+		const {contract, gateway} = await 
+			getContractAndGateway({username: user.username, chaincode: 'job', contract: 'Job'})
+				.catch(reject);
 		
 		// submit transaction
-		const rawJobs = await contract.submitTransaction('listJobs', status);
+		const rawJobs = await 
+			contract
+				.submitTransaction('listJobs', status)
+				.catch(reject);
+
 		const jobs = JSON.parse(rawJobs.toString('utf8'))
 		console.log('Transaction has been submitted');
 		
 		//disconnect
 		await gateway.disconnect();
 
-		return jobs;
-    }catch(e){ 
-		console.log('ERROR: ', e) 
-	}
+		resolve(jobs);
+		return;
+    })
 }
 
 async function get({
@@ -98,26 +81,31 @@ async function get({
 	user
 }) {
 	// Submit the specified transafalsection.
-	try{		
+	return new Promise(async (resolve, reject) => {
 		// create wallet
 		const walletPath = path.join(__dirname, '../../../../../wallet', `${user.username}.id`);
 		fs.writeFileSync(walletPath, JSON.stringify(user.wallet))
 
 		// get contract, submit transaction and disconnect
-		const {contract, gateway} = await getContractAndGateway({username: user.username, contract: 'Job'});
+		const {contract, gateway} = await 
+			getContractAndGateway({username: user.username, chaincode: 'job', contract: 'Job'})
+				.catch(reject);
 		
 		// submit transaction
-		const rawJob = await contract.submitTransaction('getJob', jobId);
+		const rawJob = await 
+			contract
+				.submitTransaction('getJob', jobId)
+				.catch(reject);
+				
 		const job = JSON.parse(rawJob.toString('utf8'))
 		console.log('Transaction has been submitted');
 		
 		//disconnect
 		await gateway.disconnect();
 
-		return job;
-    }catch(e){ 
-		console.log('ERROR: ', e) 
-	}
+		resolve(job);
+		return;
+    })
 }
 
 async function complete({
@@ -132,7 +120,7 @@ async function complete({
 
 		// get contract, submit transaction and disconnect
 		const {contract, gateway} = await 
-			getContractAndGateway({username: user.username, contract: 'Job'})
+			getContractAndGateway({username: user.username, chaincode: 'job', contract: 'Job'})
 				.catch(reject);
 		
 		// submit transaction
